c27637a70b5004f43c7a28f4131f248cb2a77ae0
user <user@mail.com>
2024-12-23 01:36:44 -0500

    Mon Dec 23 01:36:44 AM EST 2024

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 About AI-assisted tabletop roleplay
 
-./2024/12/23/README.md
 [./2024/12/23/README.md](./2024/12/23/README.md)
--- gen.sh
+++ gen.sh
@@ -6,8 +6,7 @@ echo "About AI-assisted tabletop roleplay"
 echo ""
 files=$(find ./*/ -name '*.md' | sort)
 for file in $files; do
-    htmlFile=$(echo $file | sed 's|.html|md|g')
-    echo "$htmlFile"
+    htmlFile=$(echo $file | sed 's|.md|.html|g')
     pandoc "$file" > "$htmlFile"
     echo "[$file]($file)"
 done
